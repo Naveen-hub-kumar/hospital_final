e36ccf31de2cdebe194b22598e2cf192c131cf13
naveenkumar <kumarnaveen99375@gmail.com>
2026-07-07 17:35:05 +0530

    Added all

--- hospital_management/urls.py
+++ hospital_management/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
 
@@ -33,3 +35,5 @@ urlpatterns = [
 
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
